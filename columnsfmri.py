955d6be5a3f6882994968679a698a1b3bd37e50a
dchaimow <dchaimow@users.noreply.github.com>
2019-05-13 14:05:59 +0200

    complete port of optimization simulation and results printing

--- columnsfmri.py
+++ columnsfmri.py
@@ -1,7 +1,33 @@
 import numpy as np
-from scipy.stats import norm, chi2, gamma
+from scipy.stats import norm, chi2, gamma, weibull_min
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+def printResults(results):
+    print('optimized quantity                 | optimal value | optimal voxel width')
+    print('-----------------------------------+---------------+--------------------')
+    print('univariate detection probability   | %.2f          | %.2f mm            ' %
+          (results['pDetectUnivariate_max'],results['pDetectUnivariate_optW']))
+    print('multivariate detection probability | %.2f          | %.2f mm            ' %
+          (results['pDetectMultivariate_max'],results['pDetectMultivariate_optW']))
+    print('decoding probability - 2 classes   | %.2f          | %.2f mm            ' %
+          (results['pDecode2_max'],results['pDecode2_optW']))
+    print('decoding accuracy    - 2 classes   | %.2f          | %.2f mm            ' %
+          (results['accuracyDecode2_max'],results['accuracyDecode2_optW']))
+    print('decoding probability - 4 classes   | %.2f          | %.2f mm            ' %
+          (results['pDecode4_max'],results['pDecode4_optW']))
+    print('decoding accuracy    - 4 classes   | %.2f          | %.2f mm            ' %
+          (results['accuracyDecode4_max'],results['accuracyDecode4_optW']))
+    print('decoding probability - 8 classes   | %.2f          | %.2f mm            ' %
+          (results['pDecode8_max'],results['pDecode8_optW']))
+    print('decoding accuracy    - 8 classes   | %.2f          | %.2f mm            ' %
+          (results['accuracyDecode8_max'],results['accuracyDecode8_optW']))
+    print('pattern correlation                | %.2f          | %.2f mm            ' %
+          (results['patternCorrelation_max'],results['patternCorrelation_optW']))
+
+def cdfModelWeibull(a,b,y0):
+    f = lambda x: y0+(weibull_min.cdf(x,b,scale=a)-weibull_min.cdf(0,b,scale=a))*(1-y0)
+    return f
 
 def simulatefMRIOfColumnPatterns(parameters):
     # set parameters
@@ -58,15 +84,128 @@ def simulatefMRIOfColumnPatterns(parameters):
             # add noise
             mriPlusNoisePattern = mriPattern + noiseOfW[zW] * np.random.randn(*mriPattern.shape);
             # calculate correlation to original (neuronal) pattern        
-            cor[zTrial,zW] =  sim.patternCorrelation(columnPattern,mriPlusNoisePattern);
+            cor[zTrial,zW] = sim.patternCorrelation(columnPattern,mriPlusNoisePattern);
     
-    # average over trials
-    # cr = np.mean(cr,axis=0);
-    # cor = mean(cor,axis=0);
+    # average over trials    
+    cr = np.mean(cr,axis=0)
+    cor = np.mean(cor,axis=0)
+
+    # calculate CNR and mvCNR
+    CNR = cr/noiseOfW
+    a = 0.26
+    mvCNR = CNR * nVoxelsOfW**a
     
-    # calculate CNR and mv-CNR
-    # cnr = cr/noiseOfW;
-    return cr,noiseOfW
+    # calculate univariate detection probability
+    pUniOfW = detectionProbability(CNR,1)
+    
+    # calculate multivariate detection probability
+    fPMultiDetect = cdfModelWeibull(1.9038,3.5990,0.05)
+    pMultiOfW = fPMultiDetect(mvCNR)
+    
+    # calculate decoding probability
+    fPDecode2 = cdfModelWeibull(2.0675,3.4101,0.05)
+    fPDecode4 = cdfModelWeibull(2.8451,3.4770,0.05)
+    fPDecode8 = cdfModelWeibull(3.8086,3.2733,0.05)
+
+    pDecodeOfW2 = fPDecode2(mvCNR)
+    pDecodeOfW4 = fPDecode4(mvCNR)
+    pDecodeOfW8 = fPDecode8(mvCNR)
+
+    # calculate decoding accuracy
+    fMeanClassPerf2 = cdfModelWeibull(4.7018,1.9954,1/2)
+    fMeanClassPerf4 = cdfModelWeibull(8.0900,2.1585,1/4)
+    fMeanClassPerf8 = cdfModelWeibull(15.8022,1.9954,1/8)
+
+    meanClassPerfOfW2 = fMeanClassPerf2(mvCNR)
+    meanClassPerfOfW4 = fMeanClassPerf4(mvCNR)
+    meanClassPerfOfW8 = fMeanClassPerf8(mvCNR)
+    
+    # find optima
+    idxMaxPUni = np.argmax(pUniOfW)
+    maxPUni = pUniOfW[idxMaxPUni]
+    optWPUni = wRange[idxMaxPUni]
+    
+    idxMaxMVCNR = np.argmax(mvCNR)
+    maxMVCNR = mvCNR[idxMaxMVCNR]    
+    optWMVCNR = wRange[idxMaxMVCNR]
+
+    maxPMulti = np.amax(pMultiOfW)
+    assert maxPMulti==pMultiOfW[idxMaxMVCNR]
+    
+    maxPDecode2 = np.max(pDecodeOfW2)
+    maxPDecode4 = np.max(pDecodeOfW4)
+    maxPDecode8 = np.max(pDecodeOfW8)
+    assert maxPDecode2==pDecodeOfW2[idxMaxMVCNR]
+    assert maxPDecode4==pDecodeOfW4[idxMaxMVCNR]
+    assert maxPDecode8==pDecodeOfW8[idxMaxMVCNR]
+    
+    maxMeanClassPerf2 = np.max(meanClassPerfOfW2)
+    maxMeanClassPerf4 = np.max(meanClassPerfOfW4)
+    maxMeanClassPerf8 = np.max(meanClassPerfOfW8)
+    assert maxMeanClassPerf2==meanClassPerfOfW2[idxMaxMVCNR]
+    assert maxMeanClassPerf4==meanClassPerfOfW4[idxMaxMVCNR]
+    assert maxMeanClassPerf8==meanClassPerfOfW8[idxMaxMVCNR]
+    
+    idxMaxCor = np.argmax(cor)
+    maxCor = cor[idxMaxCor]
+    optWCor = wRange[idxMaxCor]
+    
+    # assign results
+    results = dict()
+    
+    results['w']                        = wRange
+    results['cr']                       = cr
+    results['noiseLevel']               = noiseOfW
+    results['SNR']                      = 1/noiseOfW
+    results['CNR']                      = CNR
+
+    results['pDetectUnivariate']        = pUniOfW
+    results['pDetectUnivariate_max']    = maxPUni
+    results['pDetectUnivariate_optW']   = optWPUni
+
+    results['mvCNR']                    = mvCNR
+    results['mvCNR_max']                = maxMVCNR
+    results['mvCNR_opt']                = optWMVCNR
+
+    results['pDetectMultivariate']      = pMultiOfW
+    results['pDetectMultivariate_max']  = maxPMulti
+    results['pDetectMultivariate_optW'] = optWMVCNR
+
+    results['pDecode2']                  = pDecodeOfW2
+    results['pDecode2_max']              = maxPDecode2
+    results['pDecode2_optW']             = optWMVCNR
+
+    results['accuracyDecode2']           = meanClassPerfOfW2
+    results['accuracyDecode2_max']       = maxMeanClassPerf2
+    results['accuracyDecode2_optW']      = optWMVCNR
+
+    results['pDecode4']                  = pDecodeOfW4
+    results['pDecode4_max']              = maxPDecode4
+    results['pDecode4_optW']             = optWMVCNR
+
+    results['accuracyDecode4']           = meanClassPerfOfW4
+    results['accuracyDecode4_max']       = maxMeanClassPerf4
+    results['accuracyDecode4_optW']      = optWMVCNR
+
+    results['pDecode8']                  = pDecodeOfW8
+    results['pDecode8_max']              = maxPDecode8
+    results['pDecode8_optW']             = optWMVCNR
+
+    results['accuracyDecode8']           = meanClassPerfOfW8
+    results['accuracyDecode8_max']       = maxMeanClassPerf8
+    results['accuracyDecode8_optW']      = optWMVCNR
+
+    results['patternCorrelation']       = cor
+    results['patternCorrelation_max']   = maxCor
+    results['patternCorrelation_optW']  = optWCor
+    
+    # visualize results
+    # printResults(results)
+    # displayFigureA(results)
+    # displayFigureB(results)
+    # displayFigureC(results)
+
+    return results
     
 def setParameters(*args):
     """
@@ -121,7 +260,7 @@ def setParameters(*args):
     parameters['L'] = 24  # mm, for simulation/calc. of contrast range
 
     # range of voxel widths
-    parameters['wRange'] = parameters['L']/(2*np.hstack((np.arange(3,29),[30,32,34,37,40,44,53,60,69,80,96,120,160,240])))
+    parameters['wRange'] = parameters['L']/(2*np.hstack((np.arange(3,29),[30,32,34,37,40,44,48,53,60,69,80,96,120,160,240])))
     
     # pattern parameters
     parameters['rho'] = 1/1.6  # main pattern frequency = 1/(2*column spacing)
@@ -226,10 +365,10 @@ def noiseModel(V,TR,nT,differentialFlag,*args,**kwargs):
 
 def detectionProbability(cnr,N):
     if np.size(cnr)>1:
-        p = np.zeros(shape(cnr))
+        p = np.zeros(cnr.shape)
         if np.size(N)==1:
-            N = np.ones(shape(cnr)) * N
-        for z in range(size(cnr)):
+            N = np.ones(cnr.shape) * N
+        for z in range(np.size(cnr)):
             p[z] = detectionProbability(cnr[z],N[z])
     else:
         alpha = 0.05
